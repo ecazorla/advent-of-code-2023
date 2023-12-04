7c03a7e4ad158e536888127dd4b39e8b489425ec
Esteban Cazorla <ecazorla@gmail.com>
2023-12-04 06:38:32 -0500

    UPDATE day 4

--- 4/index.ts
+++ 4/index.ts
@@ -3,21 +3,14 @@ import * as fs from 'fs';
 const parseNumbers = (line: string): any[] => {
     const winning = line.split('|')[0].split(':')[1].trim().split(' ');
     const numbers = line.split('|')[1].split(' ').filter(number => number !== '');
-    return [winning, numbers, 1];
+    return [winning, numbers];
 };
 
 const mainOne = (): any => {
     const data = fs.readFileSync('data').toString().split('\n').map((line) => {
         return parseNumbers(line);
     }).map((line) => {
-        let winners = 0;
-        for (let i = 0; i < line[1].length; i++) {
-            if (line[0].includes(line[1][i])) {
-                winners++;
-            }
-        }
-
-        return winners;
+        return line[1].filter(number => line[0].includes(number)).length;
     }).filter(winner => winner !== 0).map((winner) => {
         return 2 ** (winner - 1);
     }).reduce((acc, curr) => {
@@ -31,14 +24,7 @@ const mainTwo = (): any => {
     const lines = fs.readFileSync('data').toString().split('\n').map((line) => {
         return parseNumbers(line);
     }).map((line) => {
-        let winners = 0;
-        for (let i = 0; i < line[1].length; i++) {
-            if (line[0].includes(line[1][i])) {
-                winners++;
-            }
-        }
-
-        return [winners, line[2]];
+        return [line[1].filter(number => line[0].includes(number)).length, 1];
     });
 
     for (let i = 0; i < lines.length; i++) {
